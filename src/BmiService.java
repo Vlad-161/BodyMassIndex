bcad1a59481f4b343678a11c55fa4710a12ad227
Vlad Polovinkin <polovinkinvlaaad@gmail.com>
2025-01-28 23:14:11 +0300

    Corrected code

--- src/BmiService.java
+++ src/BmiService.java
@@ -2,7 +2,7 @@ public class BmiService {
 
     public int calculate (int weight, double height) {
         double result;
-        result = weight / ((double) height * 2);
+        result = weight / ((double) height * height);
 
         return (int) result;
     }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        double height = 1.90; // метра
-        int weight = 80; // кг
+        double height = 1.87; // метра
+        int weight = 98; // кг
         int index = service.calculate(weight, height);
 
         System.out.println(index);
